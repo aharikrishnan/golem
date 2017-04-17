2c9ca66a3a2edfe5527683e8281158783796e81b
Hari <harikrishnan.a@infibeam.net>
2017-04-17 11:29:16 +0530

    Adding keyword to csv

--- src/models/amazon_product.rb
+++ src/models/amazon_product.rb
@@ -23,7 +23,8 @@ class AmazonProduct < ActiveRecord::Base
         first_leaf_bn_id = item.css('BrowseNodes > BrowseNode').select{|bn| bn.css(">Children").length == 0}
         first_leaf_bn_id = (first_leaf_bn_id.length > 0)?  first_leaf_bn_id.first.css("> BrowseNodeId").text : bn_ids.first
         info "Leaf bn => #{first_leaf_bn_id}"
-        attrs = {:title => title, :seo_url => seo_url, :model => model, :brand => brand, :upc => upc, :ean => ean, :source_id => crawl.id, :bn_id => bn_ids.first, :bn_ids => bn_ids}
+k = crawl.fields[:keywords]
+        attrs = {:k => k, :title => title, :seo_url => seo_url, :model => model, :brand => brand, :upc => upc, :ean => ean, :source_id => crawl.id, :bn_id => bn_ids.first, :bn_ids => bn_ids}
         if ( file = options[:of] ).present?
           File.open(file, 'a'){|fo| fo.write(attrs.sort.map{|a|a[1].to_s}.join("\t")); fo.write("\n") }
         end
